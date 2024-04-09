f0c26b94b637d0294db9405db8c5f99b2f876db1
Kalana Ekanayake <ekanayakekalana12@gmail.com>
2024-04-09 21:46:53 +0530

    navbar fixed

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import MenuItem from "./MenuItem";
 import Burger from "./Burger";
 import NavbarModal from "./NavbarModal";
 
-let baseClassNames =
-  "text-center uppercase py-4 sm:flex sm:flex-row sm:pb-0 items-center sm:w-3/5 md:w-1/2 lg:w-2/5 md:ml-auto border-t-4 border-white sm:border-none sm:pt-0 border-solid";
+// let baseClassNames =
+//   "text-center uppercase py-4 sm:flex sm:flex-row sm:pb-0 items-center sm:w-3/5 md:w-1/2 lg:w-2/5 md:ml-auto border-t-4 border-white sm:border-none sm:pt-0 border-solid max-[678px]:hidden";
 
 export default function Navbar() {
   const [isToggle, setIsToggle] = useState(false);
@@ -14,18 +14,14 @@ export default function Navbar() {
     setIsToggle(true);
   }
 
-  if (!isToggle) {
-    baseClassNames += " hidden";
-  }
-
   return (
-    <nav className="bg-violet-700 sm:flex sm:flex-row px-4 fixed w-full z-10">
-      <div className="flex flex-row justify-between py-4 sm:w-2/5">
+    <nav className="bg-violet-700 flex flex-row px-4 fixed w-full z-10">
+      <div className="flex flex-row justify-between py-4 sm:w-2/5 grow">
         <img src={companyLogo} alt="company logo" className="object-none" />
         <Burger onToggle={toggleHandle} />
       </div>
 
-      <ul className={baseClassNames}>
+      <ul className="text-center uppercase py-4 sm:flex sm:flex-row sm:pb-0 items-center sm:w-3/5 md:w-1/2 lg:w-2/5 md:ml-auto border-t-4 border-white sm:border-none sm:pt-0 border-solid max-sm:hidden">
         <MenuItem>
           <a href="">Services</a>
         </MenuItem>
