97363c7c318af17dae2e9f97b6ca4d442a12a96f
Kalana Ekanayake <ekanayakekalana12@gmail.com>
2024-04-09 19:11:36 +0530

    Hamburger modified

--- vite-project/src/components/Navbar/Burger.jsx
+++ vite-project/src/components/Navbar/Burger.jsx
@@ -2,13 +2,13 @@
 export default function Burger({ onToggle }) {
   return (
     <button
-      className='flex flex-col justify-around w-10 h-auto p-1 sm:hidden hover:animate-pulse transition-all'
-      type='button'
+      className="flex flex-col justify-around w-10 h-auto p-1 sm:hidden transition-all"
+      type="button"
       onClick={onToggle}
     >
-      <span className='w-full border-t-4 border-white border-solid'></span>
-      <span className='w-full border-t-4 border-white border-solid'></span>
-      <span className='w-full border-t-4 border-white border-solid'></span>
+      <span className="w-full border-t-4 border-white border-solid rounded-md"></span>
+      <span className="w-full border-t-4 border-white border-solid rounded-md"></span>
+      <span className="w-full border-t-4 border-white border-solid rounded-md"></span>
     </button>
-  )
+  );
 }
