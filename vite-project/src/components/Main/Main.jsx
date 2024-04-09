5ba264c570d27ee0a0f332cd6de26e9d276bb811
Kalana Ekanayake <ekanayakekalana12@gmail.com>
2024-04-09 15:39:06 +0530

    Hero image changed, Footer "Our Services" changed

---
+++ vite-project/src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import { SECTION_DATA } from '../../util/mainSectionData';
+import MainSectionItem from './MainSectionItem';
+
+export default function Main() {
+  return (
+    <main className='flex flex-col justify-center items-center py-16 lg:px-24 xl:px-40'>
+      {SECTION_DATA.map((item, index) => <MainSectionItem 
+        key={`${item}#${index}`}
+        img375={item.smallImage}
+        img768={item.mediumImage}
+        img1200={item.largeImage}
+        img1440={item.extraLargeImage}
+        title={item.title}
+        description={item.description}
+        index={index}
+      />)}
+    </main>
+  )
+}
---
+++ vite-project/src/components/Main/MainSectionItem.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+let imageClasses;
+let descriptionClasses;
+export default function MainSectionItem({
+  img375,
+  img768,
+  img1200,
+  img1440,
+  title,
+  description,
+  index,
+}) {
+  if (index % 2 === 0) {
+    imageClasses = `object-cover px-12 my-4 h-[275px] w-[275px] lg:h-[346px] lg:w-[346px] xl:h-[414px] xl:w-[414px] order-1 md:order-1`;
+    descriptionClasses = `flex flex-col justify-center items-center px-8 order-2 md:order-2`;
+  } else {
+    imageClasses = `object-cover px-12 my-4 h-[275px] w-[275px] lg:h-[346px] lg:w-[346px] xl:h-[414px] xl:w-[414px] order-1 md:order-2`;
+    descriptionClasses = `flex flex-col justify-center items-center px-8 order-2 md:order-1`;
+  }
+
+  return (
+    <section className="flex flex-col justify-center items-center mb-8 md:flex-row">
+      <img
+        className={imageClasses}
+        src={img1440}
+        alt="section logo"
+        srcSet={`${img375} 375w,
+           ${img768} 768w,
+           ${img1200} 1200w,
+           ${img1440} 1440w`}
+        sizes="(max-width: 767px) 375px,
+               (max-width: 1199px) 768px,
+               (max-width: 1439px) 1200px,
+               (max-width: 1579px) 1440px"
+      />
+      <div className={descriptionClasses}>
+        <h2 className="text-3xl font-bold text-violet-700 text-center mb-8">
+          {title}
+        </h2>
+        <p className="text-lg text-center mb-8">{description}</p>
+        <button
+          className="text-sm bg-orange-400 text-white rounded uppercase py-2 px-4
+          hover:scale-90 transition-all"
+        >
+          Learn More
+        </button>
+      </div>
+    </section>
+  );
+}
